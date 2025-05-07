271367c49fde2ac83bcefdc93a9ab23a1fa34e44
lauritsjelsbakthomsen <laurits@jelsbak-thomsen.dk>
2025-05-07 13:01:24 +0200

    danish labels

--- view/scripts/front.js
+++
@@ -1,9 +0,0 @@
-function post() {
-  let name = document.getElementById("name").value;
-  let ingredient = document.getElementById("ingredient").value;
-  let metode = document.getElementById("metode").value;
-
-  console.log(name);
-  console.log(ingredient);
-  console.log(metode);
-}
